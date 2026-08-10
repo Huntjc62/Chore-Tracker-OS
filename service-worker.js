9db82cbe404fe961df1130b2613fdfc61b055a78
Huntjc62 <huntjc62@gmail.com>
2026-08-10 15:49:05 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "our-home-v6-profiles";
+const CACHE = "our-home-v7-smart-notifications";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./our-home-icons/icon-192.png", "./our-home-icons/icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
