277e6694cde0de3c7f04baf8759e47644bb7c3c1
Huntjc62 <huntjc62@gmail.com>
2026-08-10 15:22:02 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,5 @@
-const CACHE = "our-home-v2-rewards";
-const ASSETS = [
-  "./our-home-firestore-pwa.html",
-  "./manifest.json",
-  "./our-home-icons/icon-192.png",
-  "./our-home-icons/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
+const CACHE = "our-home-v6-shared-rewards";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./our-home-icons/icon-192.png", "./our-home-icons/icon-512.png"];
+self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener("fetch", event => { if(event.request.method !== "GET") return; event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
